1fb4d8e33f173c537a344c7b9ee931b59ebffb0e
andydevman <andyire2005@googlemail.com>
2019-01-02 20:46:29 +0000

    loops v1

---
+++ Loops.py
@@ -0,0 +1,30 @@
+# Looking at Loops in Python
+# for loops are in the following format
+# for LOOP_VARIABLE in 
+
+
+# Objective 1 - Use a 'for loop' to print the numbers 1 - 15, using the range() function
+print("\nObjective 1 - Use 'for loop' to print numbers in range 1 - 15 using the range() function")
+for number in range(1,16):
+    print (number)
+
+
+# Objective 2 - Use a 'for loop' in conjunction with the range() function to print a list of even numbers
+print("\nObjective 2 - Use a 'for loop' in conjunction with the range() function to print a list of even numbers")
+for number in range(1,20,2):
+    print (number)
+
+# Objective 3 - Use a 'for loop' in conjunction with the range() function to print a list of even numbers
+print("\nObjective 3 - Use a 'for loop' in conjunction with the range() function to print a list of even numbers")
+for number in range(1,20,3):
+    print (number)
+
+# Objective 4 - Use a 'for loop' to print all values within a list in order
+print("\nObjective 4 - Define a list with a number of items, using a 'for loop' step through the list printing one list item on each iteration throgh the loop")
+test_list = ['Item 0', 'Item 1', 'Item 2', 'Item 3']
+for loop_Variable in test_list:
+    print (loop_Variable)
+
+
+
+# Objective 4 - Use a 
